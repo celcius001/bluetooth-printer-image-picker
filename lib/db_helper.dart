38c56afb7f325e9ed33e8ef57239192ca415738c
celcius001 <itscelciusgaming@gmail.com>
2025-04-24 14:45:44 +0800

    add signature and save signature

--- lib/db_helper.dart
+++ lib/db_helper.dart
@@ -1,4 +1,5 @@
 import "dart:io";
+import "dart:typed_data";
 import "package:path_provider/path_provider.dart";
 import "package:sqflite/sqflite.dart";
 import "package:path/path.dart";
@@ -24,17 +25,19 @@ class DatabaseHelper {
       version: 1,
       onCreate: (db, version) async {
         await db.execute(
-          "CREATE TABLE $_tableName (id INTEGER PRIMARY KEY, name TEXT, path TEXT)",
+          "CREATE TABLE $_tableName (id INTEGER PRIMARY KEY, name TEXT, path TEXT, signature BLOB)",
         );
       },
     );
   }
 
+  // Insert a new image into the database
   Future<int> insertImage(String path) async {
     final db = await database;
     return await db.insert(_tableName, {"path": path});
   }
 
+  // Insert a new image with name into the database
   Future<int> updateImage(String id, String path) async {
     final db = await database;
     return await db.update(
@@ -45,20 +48,46 @@ class DatabaseHelper {
     );
   }
 
+  // get all images from the database
   Future<List<Map<String, dynamic>>> getImages() async {
     final db = await database;
     return await db.query(_tableName);
   }
 
+  //get all images from the database with name
   Future<String?> getImageName(int id) async {
     final db = await database;
     final result = await db.query(_tableName, where: "id = ?", whereArgs: [id]);
     return result.isNotEmpty ? result.first["name"].toString() : null;
   }
 
+  // get image path by id
   Future<String?> getImageById(int id) async {
     final db = await database;
     final result = await db.query(_tableName, where: "id = ?", whereArgs: [id]);
     return result.isNotEmpty ? result.first["path"] as String : null;
+  }
+
+  // insert signature image into the database
+  Future<int> insertSignature(Uint8List bytes) async {
+    final db = await database;
+    return await db.insert(_tableName, {"signature": bytes});
+  }
+
+  Future<int> updateSignature(int id, Uint8List bytes) async {
+    final db = await database;
+    return await db.update(
+      _tableName,
+      {"signature": bytes},
+      where: "id = ?",
+      whereArgs: [id],
+    );
+  }
+
+  // get signature image by id
+  Future<Uint8List?> getSignatureById(int id) async {
+    final db = await database;
+    final result = await db.query(_tableName, where: "id = ?", whereArgs: [id]);
+    return result.isNotEmpty ? result.first["signature"] as Uint8List : null;
   }
 }
--- lib/image_capture.dart
+++ lib/image_capture.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:agmm_v3/db_helper.dart';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:signature/signature.dart';
 
 class ImageCapture extends StatefulWidget {
   const ImageCapture({super.key});
@@ -15,10 +17,16 @@ class ImageCapture extends StatefulWidget {
 class _ImageCaptureState extends State<ImageCapture> {
   final ImagePicker _picker = ImagePicker();
   final DatabaseHelper _dbHelper = DatabaseHelper();
+  final SignatureController _signatureController = SignatureController(
+    penStrokeWidth: 5,
+    penColor: Colors.black,
+  );
   TextEditingController searchController = TextEditingController();
   String? _searchImagePath;
   String? _searchImageId;
+  Uint8List? _signatureBytes;
   bool isSearching = false;
+  bool _showSignaturePad = true;
   List<Map<String, dynamic>> imageRecord = [];
 
   @override
@@ -27,12 +35,20 @@ class _ImageCaptureState extends State<ImageCapture> {
     _loadImages();
   }
 
+  /// Loads all images from the database and updates the imageRecord
+  /// state variable with the result.
   Future<void> _loadImages() async {
     List<Map<String, dynamic>> images = await _dbHelper.getImages();
     setState(() {
       imageRecord = images;
     });
   }
+
+  /// Captures an image using the device camera and inserts it into the database.
+  ///
+  /// If the image is successfully captured, it updates the local state to reflect
+  /// the new image path and sets the searching flag to true. It also reloads the
+  /// images from the database to update the image record.
 
   Future<void> _captureImage() async {
     final XFile? image = await _picker.pickImage(source: ImageSource.camera);
@@ -46,6 +62,15 @@ class _ImageCaptureState extends State<ImageCapture> {
     }
   }
 
+  /// Updates an existing image in the database by capturing a new image
+  /// using the device camera and replacing the old image path with the
+  /// new one.
+  ///
+  /// The image ID is retrieved from the searchController text field.
+  /// If the image capture is successful, the local state is updated with
+  /// the new image path and the existing image record is reloaded from
+  /// the database.
+
   Future<void> _updateImage() async {
     String id = searchController.text.trim();
     final XFile? image = await _picker.pickImage(source: ImageSource.camera);
@@ -58,6 +83,14 @@ class _ImageCaptureState extends State<ImageCapture> {
     }
   }
 
+  /// Searches the database for an image with the given path and updates the
+  /// local state with the found image path and ID, if any.
+  ///
+  /// The image path is retrieved from the searchController text field.
+  /// If the image is found in the database, the local state is updated with
+  /// the found image path and ID and the searching flag is set to true.
+  /// If the image is not found, the searching flag is set to false and the
+  /// local state is reset to null.
   Future<void> _searchByPath() async {
     String path = searchController.text.trim();
     if (path.isEmpty) {
@@ -75,6 +108,50 @@ class _ImageCaptureState extends State<ImageCapture> {
       _searchImageId = foundId;
       isSearching = true;
     });
+  }
+
+  Future<void> _saveSignature() async {
+    String id = searchController.text.trim();
+    if (_signatureController.isNotEmpty) {
+      final Uint8List? signatureBytes = await _signatureController.toPngBytes();
+      if (signatureBytes != null) {
+        await _dbHelper.updateSignature(int.parse(id), signatureBytes);
+        if (mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("Signature saved successfully!")),
+          );
+        }
+        signatureBytes.clear();
+      }
+    }
+  }
+
+  Future<void> _loadSignature() async {
+    String id = searchController.text.trim();
+    Uint8List? signatureBytes = await _dbHelper.getSignatureById(int.parse(id));
+    if (signatureBytes != null) {
+      if (mounted) {
+        setState(() {
+          _signatureBytes = signatureBytes;
+          _showSignaturePad = false;
+        });
+      }
+    } else {
+      if (mounted) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text("No signature found!")));
+      }
+    }
+  }
+
+  void _clearSignature() {
+    if (mounted) {
+      setState(() {
+        _signatureBytes = null;
+        _showSignaturePad = true;
+      });
+    }
   }
 
   @override
@@ -112,6 +189,45 @@ class _ImageCaptureState extends State<ImageCapture> {
                     SizedBox(width: 10),
                     Expanded(child: (Image.file(File(_searchImagePath!)))),
                     SizedBox(height: 10),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child:
+                          _showSignaturePad
+                              ? Signature(
+                                controller: _signatureController,
+                                height: 200,
+                                backgroundColor: Colors.white,
+                              )
+                              : _signatureBytes != null
+                              ? Image.memory(_signatureBytes!)
+                              : SizedBox.shrink(),
+                    ),
+                    SizedBox(height: 10),
+                    Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              _clearSignature();
+                            },
+                            child: Text("Clear Signature"),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              _loadSignature();
+                            },
+                            child: Text("Load Signature"),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              _saveSignature();
+                            },
+                            child: Text("Save Signature"),
+                          ),
+                        ],
+                      ),
+                    ),
                     ElevatedButton(
                       onPressed: () {
                         _updateImage();
